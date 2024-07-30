a28b095405b1d147df1575afba592fc1e302b8b9
osmannyildiz <iamosmannyildiz@gmail.com>
2024-07-31 02:28:32 +0300

    make swap form interactive

--- frontend/src/routes/index.lazy.jsx
+++ frontend/src/routes/index.lazy.jsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 import { TOKENS } from "../data";
 
 export const Route = createLazyFileRoute("/")({
@@ -6,6 +7,16 @@ export const Route = createLazyFileRoute("/")({
 });
 
 function Swap() {
+	const [sellTokenAddress, setSellTokenAddress] = useState(TOKENS[0].address);
+	const [buyTokenAddress, setBuyTokenAddress] = useState(TOKENS[1].address);
+	const [sellAmount, setSellAmount] = useState("0");
+	const [buyAmount, setBuyAmount] = useState("0");
+
+	useEffect(() => {
+		// TODO Calculate buy amount
+		setBuyAmount(sellAmount);
+	}, [sellTokenAddress, buyTokenAddress, sellAmount, buyAmount]);
+
 	const onSubmit = async (event) => {
 		event.preventDefault();
 
@@ -25,7 +36,9 @@ function Swap() {
 						<select
 							name="sellToken"
 							id="sell--token"
-							className="px-3 py-2 bg-transparent border border-black border-opacity-20 rounded-xl text-lg"
+							value={sellTokenAddress}
+							onChange={(event) => setSellTokenAddress(event.target.value)}
+							className="token-select"
 						>
 							{TOKENS.map((token) => (
 								<option key={token.address} value={token.address}>
@@ -38,34 +51,45 @@ function Swap() {
 						type="text"
 						name="sellAmount"
 						id="sell--amount"
+						value={sellAmount}
+						onChange={(event) =>
+							setSellAmount(event.target.value.replaceAll(/[^\d.]/g, ""))
+						}
 						className="bg-transparent border border-black border-opacity-20 rounded-xl text-5xl px-4 py-2 self-stretch"
-						value={123}
 					/>
 				</div>
 
 				<div className="flex flex-col gap-2">
-					<div className="flex justify-between">
+					<div className="flex justify-between items-end">
 						<label htmlFor="buy--amount" className="text-3xl">
 							Buy
 						</label>
 						<select
 							name="buyToken"
 							id="buy--token"
-							className="px-3 py-2 bg-transparent border border-black border-opacity-20 rounded-xl text-lg"
+							value={buyTokenAddress}
+							onChange={(event) => setBuyTokenAddress(event.target.value)}
+							className="token-select"
 						>
-							{TOKENS.map((token) => (
-								<option key={token.address} value={token.address}>
-									{token.name}
-								</option>
-							))}
+							{TOKENS.filter((token) => token.address !== sellTokenAddress).map(
+								(token) => (
+									<option key={token.address} value={token.address}>
+										{token.name}
+									</option>
+								)
+							)}
 						</select>
 					</div>
 					<input
 						type="text"
 						name="buyAmount"
 						id="buy--amount"
-						className="bg-transparent border border-black border-opacity-20 rounded-xl text-5xl px-4 py-2 self-stretch"
-						value={123}
+						value={buyAmount}
+						onChange={(event) =>
+							setBuyAmount(event.target.value.replaceAll(/[^\d.]/g, ""))
+						}
+						className="bg-black bg-opacity-10 border border-black border-opacity-20 rounded-xl text-5xl px-4 py-2 self-stretch cursor-not-allowed"
+						disabled
 					/>
 				</div>
 

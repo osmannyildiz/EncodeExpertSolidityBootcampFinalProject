9c908a1c4dcf0e765749df5796966d2295c1c527
Diego Aguilar <d.aguilaro99@gmail.com>
2024-07-31 13:07:43 -0500

    Updated mock token
    will make building the faucet frontend easier

--- contracts/contracts/ERC20Mock.sol
+++ contracts/contracts/ERC20Mock.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(uint256 amount) external {
+         _mint(msg.sender, amount);
     }
 }

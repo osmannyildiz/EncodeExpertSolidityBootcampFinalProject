204fe520b3831d065772cdda3bc216c9be4d8fb3
Joshua Akachi Avoaja <avoajajoshua@gmail.com>
2024-08-01 06:00:17 +0100

    feat: update script to copy files for new contracts

--- copy_evm_deployment_to_frontend.sh
+++ copy_evm_deployment_to_frontend.sh
@@ -3,3 +3,7 @@ rm ./frontend/src/evm-deployment/*
 cp "./contracts/ignition/deployments/chain-31337/deployed_addresses.json" "./frontend/src/evm-deployment/deployed_addresses.json"
 
 cp "./contracts/artifacts/contracts/Factory.sol/Factory.json" "./frontend/src/evm-deployment/Factory.json"
+
+cp "./contracts/artifacts/contracts/Pool.sol/Pool.json" "./frontend/src/evm-deployment/Pool.json"
+
+cp "./contracts/artifacts/contracts/ERC20Mock.sol/ERC20Mock.json" "./frontend/src/evm-deployment/ERC20Mock.json"
